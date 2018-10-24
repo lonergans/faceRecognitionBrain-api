b4a334c368f596ade259fd642a9163d60b562634
Lonergans <stephen.lonergan.13@gmail.com>
2018-10-24 16:07:52 +0300

    adding project to git

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '0899e326bb8b484b8be2bd67e14534a0'
+   });
+   const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => {
+      res.json(data);
+  })
+  .catch(err => res.status(400).json('unable to work with API'));
+
+}
+
+const handleImage = (req, res, db) => {
+    const {
+        id
+    } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+};
